5dadba133daaa13811a1656e7daf63b80a11e0ad
atsushimemet <noap3b69n@gmail.com>
2024-02-28 08:19:52 +0900

    chore: user_media_info all_response checking

--- debug.py
+++ debug.py
@@ -44,56 +44,47 @@ def user_info(
     return response.json()["business_discovery"]
 
 
-# メディア情報を取得する
-# def user_media_info(business_account_id, token, username, media_fields):
-#     all_response = []
+def user_media_info(business_account_id, token, username, media_fields):
+    """メディア情報を取得する"""
+    all_response = []
+    result = user_info()
+    all_response.append(result["media"]["data"])
+    print(all_response)
+    import sys
 
-#     request_url = (
-#         "https://graph.facebook.com/"
-#         + version
-#         + "/{business_account_id}?fields=business_discovery.username({username}){{media{{{media_fields}}}}}&access_token={token}".format(
-#             business_account_id=business_account_id,
-#             username=username,
-#             media_fields=media_fields,
-#             token=token,
-#         )
-#     )
-#     #     print(request_url)
-#     response = requests.get(request_url)
-#     result = response.json()["business_discovery"]
+    sys.exit()
 
-#     all_response.append(result["media"]["data"])
+    # 過去分がある場合は過去分全て取得する(1度に取得できる件数は25件)
+    if "after" in result["media"]["paging"]["cursors"].keys():
+        next_token = result["media"]["paging"]["cursors"]["after"]
+        while next_token is not None:
+            request_url = (
+                "https://graph.facebook.com/"
+                + version
+                + "/{business_account_id}?fields=business_discovery.username({username}){{media.after({next_token}){{{media_fields}}}}}&access_token={token}".format(
+                    business_account_id=business_account_id,
+                    username=username,
+                    media_fields=media_fields,
+                    token=token,
+                    next_token=next_token,
+                )
+            )
+            #             print(request_url)
+            response = requests.get(request_url)
+            result = response.json()["business_discovery"]
+            all_response.append(result["media"]["data"])
+            if "after" in result["media"]["paging"]["cursors"].keys():
+                next_token = result["media"]["paging"]["cursors"]["after"]
+            else:
+                next_token = None
 
-#     # 過去分がある場合は過去分全て取得する(1度に取得できる件数は25件)
-#     if "after" in result["media"]["paging"]["cursors"].keys():
-#         next_token = result["media"]["paging"]["cursors"]["after"]
-#         while next_token is not None:
-#             request_url = (
-#                 "https://graph.facebook.com/"
-#                 + version
-#                 + "/{business_account_id}?fields=business_discovery.username({username}){{media.after({next_token}){{{media_fields}}}}}&access_token={token}".format(
-#                     business_account_id=business_account_id,
-#                     username=username,
-#                     media_fields=media_fields,
-#                     token=token,
-#                     next_token=next_token,
-#                 )
-#             )
-#             #             print(request_url)
-#             response = requests.get(request_url)
-#             result = response.json()["business_discovery"]
-#             all_response.append(result["media"]["data"])
-#             if "after" in result["media"]["paging"]["cursors"].keys():
-#                 next_token = result["media"]["paging"]["cursors"]["after"]
-#             else:
-#                 next_token = None
-
-#     return all_response
+    return all_response
 
 
 def main():
     p_basic_info = basic_info()
     p_user_info = user_info()
+    user_media_info()
 
 
 if __name__ == "__main__":
